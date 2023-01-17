dd7979b77ed0c98d5819965f57443a0377c04487
atah <habibiatah@gmail.com>
2023-01-17 02:48:48 -0800

    completed color flipper project

--- 01-Color-Flipper/app.js
+++ 01-Color-Flipper/app.js
@@ -1,1 +1,22 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn=document.querySelector('#btn');
+const colorSpan=document.querySelector('.color');
+
+
+
+const randomIndex=()=>{
+    let RandomValue=Math.floor(Math.random()*colors.length);
+    return RandomValue;
+}
+
+console.log(randomIndex());
+
+btn.addEventListener('click',function(){
+   let color; 
+   color=colors[randomIndex()];
+   document.body.style.backgroundColor=color;
+   colorSpan.textContent=color;
+
+})
+
--- 01-Color-Flipper/hex.js
+++ 01-Color-Flipper/hex.js
@@ -1,1 +1,29 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn=document.querySelector('#btn');
+const colorSpan=document.querySelector('.color');
+
+
+const randomIndex=()=>{
+
+    let RandomValue=Math.floor(Math.random()*hex.length);
+
+    return RandomValue;
+}
+
+
+console.log(randomIndex());
+
+btn.addEventListener('click',function(){
+   let color='#'; 
+//    color=`
+//    #${hex[randomIndex()]}${hex[randomIndex()]}${hex[randomIndex()]}${hex[randomIndex()]}${hex[randomIndex()]}${hex[randomIndex()]}`;
+
+   for(let i=0;i<6;i++){
+       color+=hex[randomIndex()]
+   }
+   
+   document.body.style.backgroundColor=color;
+   colorSpan.textContent=color;
+
+})
