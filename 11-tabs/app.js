ba77c40b9803e58a59b0c35bf6d984aaebfc94fd
atah <habibiatah@gmail.com>
2023-01-20 01:14:30 -0800

    completed the tab project

---
+++ 11-tabs/app.js
@@ -0,0 +1,34 @@
+const tab_btns=document.querySelectorAll('.tab-btn');
+const articles=document.querySelectorAll('.content');
+const about=document.querySelector('.about');
+
+
+about.addEventListener('click',function(e){
+    const id=e.target.dataset.id;
+    if(id){
+
+        tab_btns.forEach(function(btn){
+            btn.classList.remove('active')
+            e.target.classList.add('active')
+        })
+
+        articles.forEach(function(article){
+           article.classList.remove('active');
+        })
+
+        const element=document.getElementById(id);
+        element.classList.add('active');
+
+       
+
+
+
+        
+
+
+
+
+
+
+    }
+})
