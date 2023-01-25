8c38ceabc1164b89802ee78ce8b2c59ed1b519e8
atah <habibiatah@gmail.com>
2023-01-25 00:41:58 -0800

    completed the Gallery prooject

--- 17-Gallery/app-class.js
+++ 17-Gallery/app-class.js
@@ -1,3 +1,16 @@
+
+function getElement(selection) {
+  const element = document.querySelector(selection);
+  if (element) {
+    return element;
+  }
+  throw new Error(
+    `Please check "${selection}" selector, no such element exists`
+  );
+}
+
+
+
 class Gallery{
 
     constructor(element){
