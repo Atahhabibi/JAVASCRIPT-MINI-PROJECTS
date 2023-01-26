358358029f2bdc045eff9468b04d68a791065d1f
atah <habibiatah@gmail.com>
2023-01-26 06:59:25 -0800

    completed dark mode project topic covers{moment.js external library for fromating date}

---
+++ 19-Dark-mode/app.js
@@ -0,0 +1,38 @@
+const articlesContainer=document.querySelector('.articles');
+const toggleBtn=document.querySelector('.btn');
+
+toggleBtn.addEventListener('click',()=>{
+    document.documentElement.classList.toggle('dark-theme')
+  })
+  
+
+const articlesData=articles.map((item)=>{
+
+    const formatDate=moment(item.date).format('MMMM Do ,YYYY')
+
+   return `<article class="post">
+
+  <h2>${item.title}</h2>
+
+  <div class="post-info">
+    <span>${formatDate}</span>
+    <span>${item.length} min read</span>
+  </div>
+
+  <p>${item.snippet}</p>
+
+</article>`
+
+}).join("");
+
+articlesContainer.innerHTML=articlesData;
+
+
+
+
+
+
+
+
+
+
