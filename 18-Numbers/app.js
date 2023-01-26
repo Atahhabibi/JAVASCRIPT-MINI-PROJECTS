c5b555046ca9dde425ed659292457a57c40fe3d1
atah <habibiatah@gmail.com>
2023-01-26 05:17:33 -0800

    completed Numbers project

---
+++ 18-Numbers/app.js
@@ -0,0 +1,39 @@
+
+const items=Array.from(document.querySelectorAll('.number'));
+
+const updateCount=(el)=>{
+   const value=parseInt(el.dataset.value);
+   const increment=Math.ceil(value/1000);
+   let intialValue=0;
+
+   const increaseCount=setInterval(() => {
+       intialValue+=increment;
+
+       if(intialValue>value){
+           el.textContent=`${value}+`;
+           clearInterval(increaseCount)
+           return;
+       }
+
+       el.textContent=`${intialValue}+`
+
+   }, 1);
+   
+
+
+}
+
+items.forEach((item)=>{
+    updateCount(item)
+
+})
+
+
+
+
+
+
+
+
+
+
